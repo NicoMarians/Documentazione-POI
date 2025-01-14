864868da2f0cab37c4a4a969160658c64b597517
Nicolò Pizzonia <pizzonianicolo@itis-molinari.eu>
2025-01-14 11:26:38 +0100

    función de tabla

---
+++ Scripts/functions.js
@@ -0,0 +1,21 @@
+const create_table = () => {
+    let binding_element;
+    let table_header; 
+    return{
+        bind_element: (new_element) => {
+            binding_element = new_element;
+        },
+        config_header: (new_element) => {
+            table_header = new_element;
+        },
+        render: () => {
+            let line = "<table class=table>";
+            line += table_data.map((Luogo) => {
+                return `<tr><td>${Luogo.nome}</td></tr>`;
+            }).join("");
+            line += "</table>";
+            binding_element.innerHTML = line; 
+        
+        }
+    }
+}
